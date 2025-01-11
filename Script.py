af383acc49f156ec2f351a2f1308c9b125e46d39
ayushsatwase <111676883+ayushsatwase@users.noreply.github.com>
2025-01-11 09:24:11 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -24,10 +24,3 @@ Nᴀᴍᴇ - {}</b>
 """
     RESTART_TXT = """
 <b>Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !
-
-📅 Dᴀᴛᴇ : <code>{}</code>
-⏰ Tɪᴍᴇ : <code>{}</code>
-🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>Asia/Kolkata</code>
-🛠️ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: <code>v2.7.1 [ Sᴛᴀʙʟᴇ ]</code></b>"""
-
-
